b2e0375a2cf618100790ef09129a72e541185c0e
Frieder Seibold <fds01@gmx.de>
2024-11-08 22:06:07 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Impressum from './Components/Impressum';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
-
+//basename='/in-resonance.net'
 function App() {
   return (
-    <BrowserRouter basename='/in-resonance.net'>
+    <BrowserRouter>
       <div className="App">
         <Header />
         <Routes bas>
