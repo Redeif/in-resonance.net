25cd7563c53c0684b799879e8e0a66dee9a6a624
Frieder Seibold <fds01@gmx.de>
2024-11-08 21:13:18 +0100

    testing

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import back_logo from '../img/InResonance_Logo_Schwarz.svg'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import back_logo from '../img/InResonance_Logo_Schwarz.svg';
+
 
 const Header = () => {
-  return (
-    <div className='header'>
-        <a href='/'><img className='headerLogo' src={back_logo} alt='In Resonance Logo'></img></a>
+    return (
+      <div className='header'>
+        <Link to='/'><img className='headerLogo' src={back_logo} alt='In Resonance Logo' /></Link>
         <div className='links'>
-            <h2>
-            <a href='/'>Homepage</a> | <a href='/about'>About me</a> | <a href='/contact'>Contact</a> | <a href='/impressum'>Imprint</a>
-            </h2>
-        </div>      
-    </div>
-  )
-}
+          <h2>
+            <Link to='/'>Homepage</Link> | <Link to='/about-me'>About me</Link> | <Link to='/contact'>Contact</Link> | <Link to='/impressum'>Imprint</Link>
+          </h2>
+        </div>
+      </div>
+    );
+  }
+  
 
 export default Header
